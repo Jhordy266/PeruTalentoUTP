88d0ccb03d7adc70aca7d368c0dd57e14ce206d9
Jhordy266 <jhordygala@gmail.com>
2024-09-24 17:24:04 -0500

    Subiendo fomularios ya creados anteriormente

---
+++ src/perutalentoutp/InicioCarga.java
@@ -0,0 +1,175 @@
+
+package perutalentoutp;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionListener;
+import javax.swing.JProgressBar;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class InicioCarga extends javax.swing.JFrame {
+    
+    private JProgressBar progressBar; // Cambiado para usar progressBar
+    private int progreso = 0;
+    private Timer timer; // Para manejar la actualización del progreso
+    
+    FondoPanel fondo = new FondoPanel();
+    
+    
+    public InicioCarga() {
+        // Configuración inicial del JFrame
+        setUndecorated(true); // Elimina el borde de la ventana
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false); // Desactiva el redimensionamiento de la ventana
+        this.setContentPane(fondo);
+        
+        initComponents();
+        this.setLocationRelativeTo(null); // centra el Jframe
+    
+        // Asigna progressBar a jProgressBar1
+        progressBar = jProgressBar1; 
+
+        // Configuración del Timer para actualizar la ProgressBar
+        timer = new Timer(50, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                progreso += 1; // Incrementa el progreso
+                progressBar.setValue(progreso); // Actualiza el valor de la ProgressBar
+                if (progreso >= 100) { // Si llega al 100%
+                    timer.stop(); // Detiene el Timer
+                    openNextForm(); // Abre el siguiente formulario
+                }
+            }
+        });
+        timer.start(); // Inicia el Timer
+
+
+       
+    }
+
+  
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel3 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new FondoPanel();
+        jProgressBar1 = new javax.swing.JProgressBar();
+
+        jLabel3.setText("jLabel3");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        jPanel1.setBackground(new java.awt.Color(33, 44, 62));
+
+        jProgressBar1.setBackground(new java.awt.Color(51, 153, 0));
+        jProgressBar1.setStringPainted(true);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(95, 95, 95)
+                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(101, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGap(0, 460, Short.MAX_VALUE)
+                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(InicioCarga.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(InicioCarga.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(InicioCarga.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(InicioCarga.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new InicioCarga().setVisible(true);
+            }
+        });
+    }
+    
+     private void openNextForm() {
+        // INICIO
+        // Cierra el formulario actual
+        this.dispose();
+        // Crea y muestra el siguiente formulario
+        Login login = new Login(); // Reemplaza con tu clase de formulario
+        login.setVisible(true);
+        // FIN
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JProgressBar jProgressBar1;
+    // End of variables declaration//GEN-END:variables
+    
+            class FondoPanel extends JPanel
+            {
+                private Image imagen;
+                
+                @Override
+                public void paint(Graphics g)
+                {
+                    imagen = new ImageIcon(getClass().getResource("/imagenes/inicio1.jpeg")).getImage();
+                    g.drawImage(imagen,0, 0, getWidth(), getHeight(),this);
+                    setOpaque(false);
+                    super.paint(g);
+                }
+            }
+
+}
