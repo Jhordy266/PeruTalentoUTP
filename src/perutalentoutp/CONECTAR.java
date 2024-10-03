98784121916853d3701656ba3f3a73ae4811ab14
Franco RM <Franco RM@FrancoRM>
2024-10-02 22:51:26 -0500

    SE AGREGO 2 CLASES NUEVAS LAS CUALES PERMITEN LA CONEXION CON MYSQL DE FORMA LOCAL Y LA OTRA VERIFICA SI SE REALIZO LA CONEXION CORRECTA CON LA BASE DE DATOS.

---
+++ src/perutalentoutp/CONECTAR.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package perutalentoutp;
+import java.sql.*;
+
+
+/**
+ *
+ * @author Franco RM
+ */
+public class CONECTAR {
+    
+static String url= "jdbc:mysql://localhost:3306/base2";
+static String user= "root";
+static String pass= "FRANCORM";
+    
+
+public static Connection conectarbase(){
+
+Connection con=null;
+try
+    {
+        con=DriverManager.getConnection(url,user,pass);
+            System.out.println("BASE DE DATOS CONECTADA -- VAMO A DARLE");                 
+    }catch (SQLException e)
+      {
+          e.printStackTrace();
+      }  
+ return con;
+    
+}
+}
---
+++ src/perutalentoutp/PRUEBAMYSQL.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package perutalentoutp;
+
+/**
+ *
+ * @author Franco RM
+ */
+public class PRUEBAMYSQL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       CONECTAR dbc= new CONECTAR();
+       
+       dbc.conectarbase();
+        
+    }
+    
+}
