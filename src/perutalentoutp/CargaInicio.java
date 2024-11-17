4243d76e20c90ebdcb8d39c3e1e01c559ab0ea4b
Deybi <Deybi@DESKTOP-M6II9IU>
2024-11-16 23:02:13 -0500

    Se corrigio el codigo de CargaInicio, se comento varios formularios para un mejor entendimiento del codigo, como a la vez se eliminaron formularios innecesarios

--- src/perutalentoutp/CargaInicio.java
+++ src/perutalentoutp/CargaInicio.java
@@ -1,57 +1,38 @@
-
 package perutalentoutp;
-import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.event.ActionListener;
 import javax.swing.JProgressBar;
 import javax.swing.Timer;
 import java.awt.event.ActionEvent;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 
 public class CargaInicio extends javax.swing.JFrame {
-    
+
     private JProgressBar progressBar; // Cambiado para usar progressBar
     private int progreso = 0;
     private Timer timer; // Para manejar la actualización del progreso
-    
-    FondoPanel fondo = new FondoPanel();
-    
-    
+
     public CargaInicio() {
-        // Configuración inicial del JFrame
-        setUndecorated(true); // Elimina el borde de la ventana
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false); // Desactiva el redimensionamiento de la ventana
-        this.setContentPane(fondo);
-        
         initComponents();
         this.setLocationRelativeTo(null); // centra el Jframe
-    
-        // Asigna progressBar a jProgressBar1
-        progressBar = jProgressBar1; 
-
+ 
+        // Asigna el progressBar a tu jProgressBar1
+        progressBar = jProgressBar1;
+ 
         // Configuración del Timer para actualizar la ProgressBar
         timer = new Timer(50, new ActionListener() {
-            @Override
+            @Override // el compilador te alertaría de inmediato sobre ese error.
             public void actionPerformed(ActionEvent e) {
-                progreso += 1; // Incrementa el progreso
+                progreso += 1; // Incrementa el valor de la variable 'progreso' en 1
                 progressBar.setValue(progreso); // Actualiza el valor de la ProgressBar
-                if (progreso >= 100) { // Si llega al 100%
-                    timer.stop(); // Detiene el Timer
+                if (progreso >= 100) { // verifica si el progres bar a llegado al 100%
+                    timer.stop();  //Si es así, detiene el Timer para que deje de ejecutarse
                     openNextForm(); // Abre el siguiente formulario
                 }
             }
         });
         timer.start(); // Inicia el Timer
 
-
-       
     }
 
-  
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -71,8 +52,10 @@ public class CargaInicio extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(33, 44, 62));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel11.setBackground(new java.awt.Color(65, 170, 174));
 
@@ -87,6 +70,8 @@ public class CargaInicio extends javax.swing.JFrame {
             .addGap(0, 400, Short.MAX_VALUE)
         );
 
+        jPanel1.add(jPanel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(558, 0, -1, -1));
+
         jPanel12.setBackground(new java.awt.Color(65, 170, 174));
 
         javax.swing.GroupLayout jPanel12Layout = new javax.swing.GroupLayout(jPanel12);
@@ -100,7 +85,10 @@ public class CargaInicio extends javax.swing.JFrame {
             .addGap(0, 400, Short.MAX_VALUE)
         );
 
+        jPanel1.add(jPanel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(55, 0, -1, -1));
+
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/talentoPeruu-removebg.png"))); // NOI18N
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(176, 50, -1, -1));
 
         jPanel14.setBackground(new java.awt.Color(65, 170, 174));
 
@@ -115,6 +103,8 @@ public class CargaInicio extends javax.swing.JFrame {
             .addGap(0, 400, Short.MAX_VALUE)
         );
 
+        jPanel1.add(jPanel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 24, -1, -1));
+
         jPanel15.setBackground(new java.awt.Color(65, 170, 174));
 
         javax.swing.GroupLayout jPanel15Layout = new javax.swing.GroupLayout(jPanel15);
@@ -127,6 +117,8 @@ public class CargaInicio extends javax.swing.JFrame {
             jPanel15Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 400, Short.MAX_VALUE)
         );
+
+        jPanel1.add(jPanel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(482, 0, -1, -1));
 
         jPanel16.setBackground(new java.awt.Color(65, 170, 174));
 
@@ -141,6 +133,8 @@ public class CargaInicio extends javax.swing.JFrame {
             .addGap(0, 400, Short.MAX_VALUE)
         );
 
+        jPanel1.add(jPanel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(93, 24, -1, -1));
+
         jPanel17.setBackground(new java.awt.Color(65, 170, 174));
 
         javax.swing.GroupLayout jPanel17Layout = new javax.swing.GroupLayout(jPanel17);
@@ -154,59 +148,13 @@ public class CargaInicio extends javax.swing.JFrame {
             .addGap(0, 400, Short.MAX_VALUE)
         );
 
-        jProgressBar1.setBackground(new java.awt.Color(0, 153, 204));
+        jPanel1.add(jPanel17, new org.netbeans.lib.awtextra.AbsoluteConstraints(17, 24, -1, -1));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(17, 17, 17)
-                .addComponent(jPanel17, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jPanel12, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jPanel16, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 63, Short.MAX_VALUE)
-                        .addComponent(jLabel1)
-                        .addGap(62, 62, 62))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(18, 18, 18)
-                        .addComponent(jProgressBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGap(18, 18, 18)))
-                .addComponent(jPanel15, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jPanel14, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jPanel11, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(21, 21, 21))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel11, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jPanel12, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jPanel15, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(0, 24, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel16, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jPanel14, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jPanel17, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addGap(27, 27, 27)
-                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(15, 15, 15))
-        );
+        jProgressBar1.setBackground(new java.awt.Color(255, 153, 51));
+        jProgressBar1.setString("");
+        jPanel1.add(jProgressBar1, new org.netbeans.lib.awtextra.AbsoluteConstraints(131, 378, 333, 31));
 
-        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 600, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -260,14 +208,14 @@ public class CargaInicio extends javax.swing.JFrame {
             }
         });
     }
-    
-     private void openNextForm() {
+
+    private void openNextForm() {
         // INICIO
         // Cierra el formulario actual
         this.dispose();
         // Crea y muestra el siguiente formulario
         Login2 login = new Login2(); // Reemplaza con tu clase de formulario
-        login.setVisible(true);
+        login.setVisible(true); //  Hace visible la instancia de 'Login2',
         // FIN
     }
 
@@ -277,26 +225,13 @@ public class CargaInicio extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel11;
     private javax.swing.JPanel jPanel12;
-    private javax.swing.JPanel jPanel13;
     private javax.swing.JPanel jPanel14;
     private javax.swing.JPanel jPanel15;
     private javax.swing.JPanel jPanel16;
     private javax.swing.JPanel jPanel17;
     private javax.swing.JProgressBar jProgressBar1;
     // End of variables declaration//GEN-END:variables
-    
-            class FondoPanel extends JPanel
-            {
-                private Image imagen;
-                
-                @Override
-                public void paint(Graphics g)
-                {
-                    imagen = new ImageIcon(getClass().getResource("/imagenes/LTP.png")).getImage();
-                    g.drawImage(imagen,0, 0, getWidth(), getHeight(),this);
-                    setOpaque(false);
-                    super.paint(g);
-                }
-            }
+
+  
 
 }
